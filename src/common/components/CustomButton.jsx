53aa86f0b3119b7f1f534be2620d791dd120aaef
Austin Howard <ahoward2@unca.edu>
2020-10-21 00:10:24 -0400

    1.1S

---
+++ src/common/components/CustomButton.jsx
@@ -0,0 +1,8 @@
+import Button from '@material-ui/core/Button';
+import React from 'react';
+
+const CustomButton = () => {
+    return <Button color="primary" variant="contained" data-testid="custom-button">Welcome To CodeRoad</Button>;
+}
+
+export default CustomButton;
