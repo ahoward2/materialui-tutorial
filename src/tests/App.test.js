4c2003d08f35484aa031b6ce5c922c43e21f93db
Austin Howard <ahoward2@unca.edu>
2020-10-21 00:10:04 -0400

    1.1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,3 +1,17 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+
 test("Tests working", () => {
   expect(1 + 1).toBe(2);
+});
+
+test("CustomButton has text `Welcome To CodeRoad`", () => {
+  const { queryByTestId } = render(<App />);
+  const buttonElement = queryByTestId("custom-button");
+        if (!buttonElement){
+            throw new Error(`Custom button does not exist`)
+        }
+
+        expect(buttonElement.hasAttribute("class")).toBeTruthy();
 });
